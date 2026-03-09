dc1b569591c95f2b38a6045c4224b12fe681269d
18maldavdiya <hanish2005b@gmail.com>
2026-03-09 12:21:21 +0530

    more about components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,29 @@
 function App() {
   return (
     <div>
-      <Hi></Hi>
+      <h1>List of fruits</h1>
+      <Fruit></Fruit>
+      <h1>list of colors</h1>
+      <Color></Color>
     </div>
      
   )
 }
-function Hi(){
+function Fruit(){
   return(
     <div>
-      <h1>hi hanish</h1>
-      <h2>how can i help you?</h2>
+      <h1>Apple</h1>
+      <h2>Mango</h2>
     </div>
+  )
+}
+function Color(){
+  return(
+    <div>
+      <h1>Red</h1>
+      <h2>Yellow</h2>
+    </div>
+    
   )
 }
 
