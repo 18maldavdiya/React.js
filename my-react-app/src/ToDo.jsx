f2b7be4fd27fd6d885a0f6830a82a0cbbefe9459
18maldavdiya <hanish2005b@gmail.com>
2026-03-10 11:07:06 +0530

    jsx

---
+++ my-react-app/src/ToDo.jsx
@@ -0,0 +1,12 @@
+function Todo(){
+    return(
+        <div>
+            <ul>
+                <li>go to marker</li>
+                <li>go to school</li>
+                <li>go to work</li>
+            </ul>
+        </div>
+    )
+}
+export default Todo;
