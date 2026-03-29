3617e3c1bc86506a0db86f5ea9c4987ab1a2f9e5
18maldavdiya <hanish2005b@gmail.com>
2026-03-29 18:41:29 +0530

    props

--- react_propes/my-app/src/Product.jsx
+++ react_propes/my-app/src/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css";
 
-function Product({title, price}) {
+function Product({title, price,features}) {
+  
   return (
     <div className="Product">
       <h3>{title}</h3>
       <h2>price : {price}</h2>
+      <p>{features}</p>
     </div>
   );
 }
--- react_propes/my-app/src/ProductTab.jsx
+++ react_propes/my-app/src/ProductTab.jsx
@@ -1,9 +1,10 @@
 import Product from "./Product"
 function ProductTab(){
+    let features = ["hi -tech" , "good-quality" ,"best in range"]
     return(
     <>
-     <Product title = "phone" price = {10000}/>
-     <Product title = "laptop" price = {200000}/>
+     <Product title = "phone" price = {10000} features={features}/>
+     <Product title = "laptop" price = {200000} features ={features}/>
      
     </>
     )
