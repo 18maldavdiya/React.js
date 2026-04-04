f11ddcb29aa75e77b83d9f9b60d1816af4c2a3aa
18maldavdiya <hanish2005b@gmail.com>
2026-04-04 17:00:07 +0530

    lodoBoard

--- State Activitys/my-app/src/App.jsx
+++ State Activitys/my-app/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import LudoBoard from './LudoBoard'
 import './App.css'
 function App(){
   return(
-    
+    <div>
+      <LudoBoard/>
+    </div>
   )
 }
 
