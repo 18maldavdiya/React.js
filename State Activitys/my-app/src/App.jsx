eb51ce73664438e4ae56c038ecf93ef48486c340
18maldavdiya <hanish2005b@gmail.com>
2026-04-05 09:56:44 +0530

    add on to do

--- State Activitys/my-app/src/App.jsx
+++ State Activitys/my-app/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App(){
   return(
     <div>
-      <LudoBoard/>
+      <Create_To_do/>
     </div>
   )
 }
--- State Activitys/my-app/src/Create_To_do.jsx
+++ State Activitys/my-app/src/Create_To_do.jsx
@@ -1,7 +1,14 @@
 function Create_To_do(){
     return(
         <div>
-
+            <input placeholder="Add a task"></input>
+             <br></br>
+            <button>Add task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <h3>Take todo</h3>
         </div>
     )
 }
