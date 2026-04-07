10be8d3c3b1e0baaffe517330715827e0dcefe4a
18maldavdiya <hanish2005b@gmail.com>
2026-04-07 18:31:04 +0530

    Lottry ticket

---
+++ Lottry game/my-papp/src/Lottry.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function Lottry(){
+    let [ticket, setTicket] = useState([0 , 0 ,0]);
+    return(
+        <div>
+            <h2>Lottry Game</h2>
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
+        </div>
+    )
+}
+export default Lottry;
