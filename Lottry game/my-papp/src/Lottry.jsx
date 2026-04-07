ccb51cd6c97bc19cd220fc12fc67eef2470dc637
18maldavdiya <hanish2005b@gmail.com>
2026-04-07 18:58:00 +0530

    gen rendom

--- Lottry game/my-papp/src/Lottry.jsx
+++ Lottry game/my-papp/src/Lottry.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import "./Lottry.css";
-import {genTicket} from "./helper";
+import {genTicket,sum} from "./helper";
 
 function Lottry(){
     let [ticket, setTicket] = useState(genTicket(3));
+    let iswinning  = sum(ticket) ===15;
+    let buyTicket = () =>{
+        setTicket(genTicket(3));
+    }
     return(
         <div>
             <h2>Lottry Game</h2>
@@ -12,7 +16,10 @@ function Lottry(){
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <button onClick={buyTicket}>Buy new Ticket</button>
+            <h3>{iswinning && "you won !"}</h3>
         </div>
+        
     )
 }
 export default Lottry;
