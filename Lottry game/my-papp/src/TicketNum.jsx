c6accc435f6f0cd788da58f78ada9271881f552e
18maldavdiya <hanish2005b@gmail.com>
2026-04-08 18:22:02 +0530

    TicketNum.css

--- Lottry game/my-papp/src/TicketNum.jsx
+++ Lottry game/my-papp/src/TicketNum.jsx
@@ -1,6 +1,7 @@
+import "./TicketNum.css";
 function TicketNum({ nums }) {
     return (
-        <span>{nums}</span>
+        <span className="TicketNum">{nums}</span>
     )
 }
 export default TicketNum;
