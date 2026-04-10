b401663dc0bcc5abd66d702a1da4f20769e1ba41
18maldavdiya <hanish2005b@gmail.com>
2026-04-10 16:54:49 +0530

    lablse

--- Form in react/my-app/src/Form.jsx
+++ Form in react/my-app/src/Form.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
+import "./App.css";
 
 function Form(){
-    let [fulName ,setFulNaame] = useState("hanish");
+    let [fulName ,setFulNaame] = useState("");
+    let [userName ,setUserNaame] = useState("");
 
     let handleNameChange = (event) =>{
         setFulNaame(event.target.value);
     }
+     let handleUsername = (event) =>{
+        setUserNaame(event.target.value);
+    }
     return (
         
         <form>
-            <lable htmlFor="username">Full Name</lable>
+            
+            <lable htmlFor="fullname">Full Name</lable>
             <input type="text" placeholder="Enter your full name" 
             value={fulName} 
             onChange={handleNameChange}
+            id="fullname"
+            />
+            <br></br>
+            <br></br>
+            <lable htmlFor="username">User name</lable>
+            <input type="text" placeholder="Enter your full name" 
+            value={userName} 
+            onChange={handleUsername}
             id="username"
             />
+            
             <button>Submit</button>
         </form>
     )
