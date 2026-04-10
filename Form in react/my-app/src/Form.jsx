b553782755390a2917b60efe54f12a084ecb2ee6
18maldavdiya <hanish2005b@gmail.com>
2026-04-10 16:34:48 +0530

    lable

--- Form in react/my-app/src/Form.jsx
+++ Form in react/my-app/src/Form.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function Form(){
+    let [fulName ,setFulNaame] = useState("hanish");
+
+    let handleNameChange = (event) =>{
+        setFulNaame(event.target.value);
+    }
     return (
+        
         <form>
-            <input type="text" placeholder="Enter your full name"/>
+            <lable htmlFor="username">Full Name</lable>
+            <input type="text" placeholder="Enter your full name" 
+            value={fulName} 
+            onChange={handleNameChange}
+            id="username"
+            />
+            <button>Submit</button>
         </form>
     )
 }
