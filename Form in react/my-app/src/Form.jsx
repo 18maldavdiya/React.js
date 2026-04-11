eaf7050715dc4db521556990acccaafea6b7ad7d
18maldavdiya <hanish2005b@gmail.com>
2026-04-11 18:50:45 +0530

    form

--- Form in react/my-app/src/Form.jsx
+++ Form in react/my-app/src/Form.jsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 function Form(){
-    let [fulName ,setFulNaame] = useState("");
-    let [userName ,setUserNaame] = useState("");
+    // let [fulName ,setFulNaame] = useState("");
+    // let [userName ,setUserNaame] = useState("");
+    let [formdata,setFormData] = useState({
+        fulName: "",
+        userName: ""
+    });
 
-    let handleNameChange = (event) =>{
-        setFulNaame(event.target.value);
-    }
-     let handleUsername = (event) =>{
-        setUserNaame(event.target.value);
-    }
+    // let handleNameChange = (event) =>{
+    //     setFulNaame(event.target.value);
+    // }
+    //  let handleUsername = (event) =>{
+    //     setUserNaame(event.target.value);
+    // }
     return (
         
         <form>
             
             <lable htmlFor="fullname">Full Name</lable>
             <input type="text" placeholder="Enter your full name" 
-            value={fulName} 
+            value={formdata.fulName} 
             onChange={handleNameChange}
             id="fullname"
             />
@@ -25,7 +29,7 @@ function Form(){
             <br></br>
             <lable htmlFor="username">User name</lable>
             <input type="text" placeholder="Enter your full name" 
-            value={userName} 
+            value={formdata.userName} 
             onChange={handleUsername}
             id="username"
             />
