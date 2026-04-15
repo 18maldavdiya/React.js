ac57e953f69b0275e5af344060237586484202a2
18maldavdiya <hanish2005b@gmail.com>
2026-04-15 18:33:26 +0530

    comment

--- Form in react/my-app/src/Comment.jsx
+++ Form in react/my-app/src/Comment.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function Comment(){
+    let [comments , setComments] = useState([{
+        username : "@han",
+        remarks: "good product",
+        rating : 4
+    }])
     return(
         <div>
             <h3>All comments</h3>
+            <div className="comment">
+                <span>{comments[0].remarks}</span>
+                &bnsp;
+                <span>({comments[0].rating})</span>
+            </div>
         </div>
     )
 }
